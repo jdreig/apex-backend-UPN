1cbcc086e702db3935db2de0c11a25e46abccad3
jdreig <dreig.jdsc.1991@gmail.com>
2026-09-18 06:23:17 -0500

    Fix: actualizacion de configuracion CORS para produccion 2

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")           // Rutas que se permitirán
-                .allowedOrigins("http://localhost:4200","https://*.onrender.com")  // Origen permitido (puedes agregar más URLs si lo necesitas)
+                .allowedOrigins("http://localhost:4200","https://*.render.com")  // Origen permitido (puedes agregar más URLs si lo necesitas)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  // Métodos permitidos
                 .allowedHeaders("*")   // Todos los headers permitidos
                 .allowCredentials(true)  // Permite enviar credenciales (cookies, authorization headers)
